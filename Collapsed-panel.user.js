9c6a4ec7fd736a1ecf8057d204a1743372a7d387
mrAntiUser <mr.AntiUser@gmail.com>
2019-03-19 15:41:36 +0300

    auto collapse

--- Collapsed-panel.user.js
+++ Collapsed-panel.user.js
@@ -2,7 +2,7 @@
 // @name         Collapsed panel
 // @license      MIT
 // @namespace    argustelecom.ru
-// @version      1.0
+// @version      1.1
 // @description  Collapsed panel
 // @author       Andy BitOff
 // @include      *support.argustelecom.ru*
@@ -11,24 +11,32 @@
 // @run-at       document-start
 // ==/UserScript==
 
+/* RELEASE NOTES
+  1.1
+    Auto collapse when <maxLines> line or more.
+      <maxLines> default 15
+  1.0
+    Release
+*/
 
 (function (MutationObserver) {
   'use strict';
 
-  const startCollapsed = false;
+  // true  - collapse all on start
+  // false - expand all on start
+  // null  - auto. Collapse when <maxLines> line or more
+  const startCollapsed = null;
+  const maxLines = 15;
 
-  let $observContainer;
-  let $panels;
+  let $obsrvContainer, $panels;
   const observer = new MutationObserver(mutationCallback);
-  const startHeaderClass = (startCollapsed) ? 'cp-collapsed-head' : 'cp-expanded-head';
-  const startContentClass = (startCollapsed) ? 'cp-collapsed-content' : 'cp-expanded-content';
 
   const timId = setInterval(function() {
     if ($('body').length === 0){ return };
     clearInterval(timId);
     new MutationObserver(function(){
-      $observContainer = $('div#activitymodule div.mod-content div#issue_actions_container');
-      if ($observContainer.length === 0){ return };
+      $obsrvContainer = $('div#activitymodule div.mod-content div#issue_actions_container');
+      if ($obsrvContainer.length === 0){ return };
       this.disconnect();
       addNewCss();
       observerStart();
@@ -43,26 +51,46 @@
   function observerStart() {
     $panels = $('div.code.panel:has(div.codeHeader.panelHeader):not(.aui-button)');
     makePanels();
-    observer.observe($observContainer.get(0), {childList: true})
+    observer.observe($obsrvContainer.get(0), {childList: true})
   }
 
   function makePanels() {
     $panels.each(function(){
       const $pnlHeader = $(this).find('>div.codeHeader.panelHeader');
       if ($pnlHeader.hasClass('cp-header')){return};
-      $pnlHeader.addClass('aui-button cp-header');
-      const $pnlHeaderImg = $pnlHeader.prepend(`<div class="cp-header-img ${startHeaderClass}">»</div>`).find('div.cp-header-img');
       const $pnlContent = $(this).find('div.codeContent.panelContent');
       $pnlContent.addClass('cp-pnl-content');
       const $preContent = $pnlContent.find('> pre');
-      $preContent.height($pnlContent.height()).addClass('cp-pre-content ' + startContentClass);
+      $pnlHeader.addClass('aui-button cp-header');
+      const $pnlHeaderImg = $pnlHeader.prepend(`<div class="cp-header-img">»</div>`).find('div.cp-header-img');
+      $preContent.height($pnlContent.height()).addClass('cp-pre-content');
       $preContent.resize(function(){$pnlContent.height($preContent.height())});
-      $pnlHeader.click(function(){
-        $preContent.toggleClass('cp-expanded-content cp-collapsed-content');
-        $pnlHeaderImg.toggleClass('cp-expanded-head cp-collapsed-head');
-      })
+      $pnlHeader.click(function(){togglePanel($pnlHeaderImg, $preContent)});
+      if (startCollapsed === null){
+        togglePanel($pnlHeaderImg, $preContent, (($preContent.text() || '').match(/\n/gmi) || []).length >= maxLines);
+      } else {
+        togglePanel($pnlHeaderImg, $preContent, startCollapsed);
+      }
     });
+  }
 
+  function togglePanel($header, $content, collapse) {
+    if (collapse === undefined){
+      $header.toggleClass('cp-expanded-head cp-collapsed-head');
+      $content.toggleClass('cp-expanded-content cp-collapsed-content');
+    } else {
+      if (collapse){
+        $header.removeClass('cp-expanded-head');
+        $content.removeClass('cp-expanded-content');
+        $header.addClass('cp-collapsed-head');
+        $content.addClass('cp-collapsed-content');
+      } else {
+        $header.removeClass('cp-collapsed-head');
+        $content.removeClass('cp-collapsed-content');
+        $header.addClass('cp-expanded-head');
+        $content.addClass('cp-expanded-content');
+      }
+    }
   }
 
   function newCssClass(cssClass){
